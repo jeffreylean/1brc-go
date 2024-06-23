4e777158d38a62246055b4b286c1e93470b8e0eb
JeffreyLean <leanwf1117@gmail.com>
2024-06-23 16:21:10 +0800

    Fix bufio read bug

--- main.go
+++ main.go
@@ -7,15 +7,14 @@ import (
 	"io"
 	"os"
 	"sort"
-	"strconv"
 	"time"
 )
 
 type Station struct {
 	station string
-	min     float64
-	max     float64
-	sum     float64
+	min     int32
+	max     int32
+	sum     int64
 	count   int
 }
 
@@ -27,11 +26,12 @@ func main() {
 	}
 	defer file.Close()
 
-	buff := make([]byte, 100)
+	// 1mb of buffer
+	buff := make([]byte, 1024*1024)
 	//scanner := bufio.NewScanner(file)
 	reader := bufio.NewReader(file)
 
-	result := make(map[string]Station)
+	result := make(map[string]*Station)
 	stations := make([]string, 0)
 	pointer := 0
 
@@ -47,6 +47,7 @@ func main() {
 		chunk := buff[:pointer+n]
 		lastLineIdx := bytes.LastIndexByte(chunk, '\n')
 		if lastLineIdx < 0 {
+			fmt.Println("lastline", lastLineIdx, n)
 			break
 		}
 
@@ -55,56 +56,62 @@ func main() {
 
 		for {
 			// Split the bytes
-			splits := bytes.Split(chunk, []byte("\n"))
-			for i := 0; i < len(splits); i++ {
-				station, temp, found := bytes.Cut(splits[i], []byte(";"))
-				if !found {
-					break
-				}
-				stationString := string(station)
-				tempFloat, _ := strconv.ParseFloat(string(temp), 64)
-
-				if st, exist := result[stationString]; exist {
-					st.min = min(st.min, tempFloat)
-					st.max = max(st.max, tempFloat)
-					st.sum += tempFloat
-					st.count++
-				} else {
-					stationString := string(station)
-					result[stationString] = Station{station: stationString, min: tempFloat, max: tempFloat, sum: tempFloat, count: 1}
-					stations = append(stations, stationString)
-				}
-
+			station, after, found := bytes.Cut(chunk, []byte(";"))
+			if !found {
+				break
 			}
-			pointer = copy(buff, remaining)
+			stationString := string(station)
+			index := 0
+			isNegative := false
+
+			// Check the sign of the decimal
+			if after[index] == '-' {
+				isNegative = true
+				index++
+			}
+
+			temp := int32(0)
+			for after[index] >= '0' && after[index] <= '9' {
+				temp = temp*10 + int32(after[index]-'0')
+				index++
+			}
+			// At this point the index shoud be `.`
+			if after[index] != '.' {
+				fmt.Println(string(after[index]))
+				panic("Invalid input format")
+			}
+			// Skip `.`
+			index++
+			// Get the last digit
+			temp = temp*10 + int32(after[index]-'0')
+			if isNegative {
+				temp = -temp
+			}
+			// Skip the `\n`
+			index += 2
+			// Update chunk
+			chunk = after[index:]
+
+			if st, exist := result[stationString]; exist {
+				st.min = min(st.min, temp)
+				st.max = max(st.max, temp)
+				st.sum += int64(temp)
+				st.count++
+			} else {
+				stationString := string(station)
+				result[stationString] = &Station{station: stationString, min: temp, max: temp, sum: int64(temp), count: 1}
+				stations = append(stations, stationString)
+			}
 		}
-
+		pointer = copy(buff, remaining)
 	}
-
-	//for scanner.Scan() {
-	//	line := scanner.Text()
-	//	splits := strings.Split(line, ";")
-
-	//	station := splits[0]
-	//	temp, _ := strconv.ParseFloat(splits[1], 64)
-
-	//	if st, exist := result[station]; exist {
-	//		st.min = min(st.min, temp)
-	//		st.max = max(st.max, temp)
-	//		st.sum += temp
-	//		st.count++
-	//	} else {
-	//		result[station] = Station{station: station, min: temp, max: temp, sum: temp, count: 1}
-	//		stations = append(stations, station)
-	//	}
-	//}
 
 	// Print the result
 	sort.Strings(stations)
 	print("{")
 	for _, s := range stations {
 		r := result[s]
-		fmt.Printf("%s:%.1f/%.1f/%.1f;\n", s, r.min, r.sum/float64(r.count), r.max)
+		fmt.Printf("%s:%.1f/%.1f/%.1f;\n", s, float64(r.min)/10, float64(r.sum)/float64(10)/float64(r.count), float64(r.max)/10)
 	}
 	print("}\n")
 
